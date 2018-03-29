9154deccf95d74df31be704bb37fb74bff49b8d8
viqueen <hasnae@viqueen.org>
2018-03-30 00:49:43 +0200

    enhanced-maps: welcome empty map definition

--- src/main/java/org/viqueen/javazone/EnhancedMaps.java
+++ src/main/java/org/viqueen/javazone/EnhancedMaps.java
@@ -4,7 +4,9 @@ package org.viqueen.javazone;
 public class EnhancedMaps {
 
     public static void main (String... args) {
-
+//        // empty map definition
+//        final Map empty  = {};
+//        System.out.println(`empty map : ${empty}`);
     }
 
 }
