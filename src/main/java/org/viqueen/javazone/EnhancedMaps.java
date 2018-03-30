edca3dd23610cf921c65fc193235394c3f8d78e9
viqueen <hasnae@viqueen.org>
2018-03-30 12:56:50 +0200

    enhanced-maps: welcome singleton map

--- src/main/java/org/viqueen/javazone/EnhancedMaps.java
+++ src/main/java/org/viqueen/javazone/EnhancedMaps.java
@@ -1,12 +1,15 @@
 package org.viqueen.javazone;
 
-
 public class EnhancedMaps {
 
     public static void main (String... args) {
 //        // empty map definition
 //        final Map empty  = {};
 //        System.out.println(`empty map : ${empty}`);
+
+//        // singleton map
+//        final Map singleton = { "name": "Hasnae R" };
+//        System.out.println(`singleton map : ${singleton}`);
     }
 
 }
