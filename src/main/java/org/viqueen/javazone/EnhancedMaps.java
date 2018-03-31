e1eaff3697fb83773e3ffa89238cb90db46f903b
viqueen <hasnae@viqueen.org>
2018-03-31 21:30:03 +0200

    enhanced-maps: welcome multi entry map/dictionary example

--- src/main/java/org/viqueen/javazone/EnhancedMaps.java
+++ src/main/java/org/viqueen/javazone/EnhancedMaps.java
@@ -10,6 +10,14 @@ public class EnhancedMaps {
 //        // singleton map
 //        final Map singleton = { "name": "Hasnae R" };
 //        System.out.println(`singleton map : ${singleton}`);
+
+//        // multi entry map
+//        final Map data = {
+//                "name": "Hasnae R.",
+//                "age": "mind your own business FFS",
+//                "city": "Sydney",
+//        };
+//        System.out.println(`all the relevant info : ${data}`);
     }
 
 }
